3c3cff7a89c5f9634f89486c193e1a09d60b9875
Clayton Condie <clayton.condie@gmail.com>
2016-02-04 15:08:03 -0700

    Intial CVH class, Inital SplitAndCombine method, Intial splitting function

--- convex-hull/ConvexHullSolver.cs
+++ convex-hull/ConvexHullSolver.cs
@@ -32,23 +32,34 @@ namespace _2_convex_hull
         public void Solve(List<System.Drawing.PointF> pointList)
         {
             // TODO: Insert your code here
+
             //Sort points from left to right
-            List<System.Drawing.PointF> sortedPoints = pointList.Sort();
-            foreach(System.Drawing.PointF point in pointList)
-            {
+            pointList.Sort(new PointComparitor());
+
+            ConvexHull final = SplitAndCombine(pointList);
+
+        }
+
+        public ConvexHull SplitAndCombine(List<System.Drawing.PointF> points)
+        {
+            //DIAG: output the points
+            foreach (System.Drawing.PointF point in points)
                 Console.WriteLine(point.ToString());
+            Console.WriteLine();
+
+            //If there is only one point in the list of points, we have reached base case, return a CVH with just that point
+            if (points.Count == 1)
+            {
+                return new ConvexHull(points);
             }
 
-        }
+            //Splt the points collection into two halves
+            ConvexHull left = SplitAndCombine(points.GetRange(0, (int)Math.Floor((double)points.Count / 2)));
+            ConvexHull right = SplitAndCombine(points.GetRange((int)Math.Floor((double)points.Count / 2), (int)Math.Ceiling((double)points.Count / 2)));
 
-        public void SolveCVHLeft(List<System.Drawing.PointF> pointList)
-        {
 
-        }
 
-        public void SolveCVHRight()
-        {
-
+            return null;
         }
 
         public void MergeCVH(Object leftCVH, Object rightCVH)
@@ -56,6 +67,47 @@ namespace _2_convex_hull
 
         }
 
+        class PointComparitor : IComparer<PointF>{
+            public int Compare(PointF x, PointF y){
+                if (x.X < y.X)
+                    return -1;
+                else
+                    return 1;
+            }
+        }
+    }
+
+    class ConvexHull
+    {
+        PointF[] points;
+        int size;
+        int currentLoc;
+
+        public ConvexHull(List<PointF> pointsList)
+        {
+            size = pointsList.Count;
+            points = new PointF[size];
+            currentLoc = 0;
+
+            for (int i = 0; i < pointsList.Count; i++)
+                points[i] = pointsList[i];
+        }
+
+        //Advances the current location index by one (circular) and returns the Point Object at current Location
+        public PointF advance()
+        {
+            if (currentLoc == (size - 1))
+                currentLoc = 0;
+            else
+                currentLoc++;
+
+            return points[currentLoc];
+        }
+
+        public PointF current()
+        {
+            return points[currentLoc];
+        }
 
     }
 }
